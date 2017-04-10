5f2d5311bd99220d42ef89ee4feddb8ad05c272b
Ziyan Ma <ziyanma@usc.edu>
2017-04-09 17:47:03 -0700

    Scrum2 1

--- Backend/src/MasterLinkClass.php
+++ Backend/src/MasterLinkClass.php
@@ -1,9 +1,9 @@
 <?php
 require_once 'DataStoreClass.php';
 require_once 'WebCrawler.php';
-class MasterLink 
+class MasterLinkClass 
 { 
-	public $mdataStore;
+	public $mDataStore;
 	public $mWordMap;
 	public $query;
 	public $ACMCrawler;
@@ -11,22 +11,34 @@ class MasterLink
 
     public function  __construct()
     {
-
+        $this->mDataStore = new DataStoreClass();
+        $this->ACMCrawler = new WebCrawler("ACM");
+        $this->IEEECrawler = new WebCrawler("IEEE");
     }
 
-    public function searchQuery($query, $library)
+    public function searchQuery($query)
     {
-
+        {   
+            $url = "http://dl.acm.org/results.cfm?query=".$query;
+            // $url = 
+            $this->ACMCrawler->getContent($url);
+            $paperList = $this->ACMCrawler->getPaperContent();
+            foreach ($paperList as $paper)
+            {
+                $this->mDataStore->addPaper($paper);            
+            }
+        }
 
     }
     public function getDataStore()
     {
-    	return $this->mdataStore;
+    	return $this->mDataStore;
+
     }
 
     public function getWordMap()
     {
-    	return $this->mWordMap;
+    	return $this->mDataStore->returnMostFrequentWords();
     }
 
 
