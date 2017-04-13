fe7817c4cc38ad69628c4918b532e86d425d9148
Michael Bachmann <jbachman@usc.edu>
2017-04-13 16:42:35 -0700

    eslint

--- Frontend/dev/js/components/article-item.js
+++ Frontend/dev/js/components/article-item.js
@@ -1,156 +1,182 @@
-import React from 'react';
+import React from 'react'
 import * as ReactBootstrap from 'react-bootstrap'
-import Highlight from 'react-highlighter';
-import {IndexLink} from 'react-router';
-import '../../../dev/styles/article-item.sass';
+import Highlight from 'react-highlighter'
+import { IndexLink } from 'react-router'
+import '../../../dev/styles/article-item.sass'
 
 export default class ArticleItem extends React.Component {
-	constructor(props) {
-    super(props);
+  constructor(props) {
+    super(props)
     this.state = {
       showModal: false,
-			showAbstract: false,
-	    checked: false
-    };
+      showAbstract: false,
+      checked: false
+    }
+    this.openBibtex = this.openBibtex.bind(this)
+    this.closeBibtex = this.closeBibtex.bind(this)
+    this.openAbstract = this.openAbstract.bind(this)
+    this.closeAbstract = this.closeAbstract.bind(this)
+    this.handleCheckbox = this.handleCheckbox.bind(this)
   }
-	openAbstract() {
-		this.props.fetchAbstract();
-		this.setState({ showAbstract: true });
-	}
-	closeAbstract() {
-		this.props.clearAbstract();
-		this.setState({ showAbstract: false });
-	}
+  openAbstract() {
+    this.props.fetchAbstract()
+    this.setState({ showAbstract: true })
+  }
+  closeAbstract() {
+    this.props.clearAbstract()
+    this.setState({ showAbstract: false })
+  }
   openBibtex() {
-	  this.props.fetchBibtex();
-    this.setState({ showModal: true });
+    this.props.fetchBibtex()
+    this.setState({ showModal: true })
   }
-	closeBibtex() {
-		this.props.clearBibtex();
-		this.setState({ showModal: false });
-	}
-	handleCheckbox() {
-		const checked = this.state.checked;
-		this.setState({checked: !checked});
-		this.props.onChange(this.props.article.doi, !checked);
-	}
+  closeBibtex() {
+    this.props.clearBibtex()
+    this.setState({ showModal: false })
+  }
+  handleCheckbox() {
+    const checked = this.state.checked
+    this.setState({ checked: !checked })
+    this.props.onChange(this.props.article.doi, !checked)
+  }
 	// TODO: Switch "Smith" with author
-	authorRoute(author) {
-		this.props.addToHistory("Smith", 5);
-		this.props.generatePapers("Smith");
-		//this.props.addToHistory(author, 5);
-		//this.props.generatePapers(author);
-	}
+	/*
+	this.props.addToHistory(author, 5);
+	this.props.generatePapers(author);
+	*/
+  authorRoute(author) {
+    this.props.addToHistory('Smith', 5)
+    this.props.generatePapers('Smith')
+  }
 	// TODO: Switch "Smith" with conference
-	conferenceRoute(conference) {
-		this.props.addToHistory("Smith", 5);
-		this.props.generatePapers("Smith");
-		//this.props.addToHistory(conference, 5);
-		//this.props.generatePapers(conference);
-	}
+	/*
+	this.props.addToHistory(conference, 5);
+	this.props.generatePapers(conference);
+	*/
+  conferenceRoute(conference) {
+    this.props.addToHistory('Smith', 5)
+    this.props.generatePapers('Smith')
+  }
 
-	render() {
-		const { authors, conferences, downloadLink, title, doi, frequency } = this.props.article;
-		const { bibtex } = this.props.bibtexData.bibtex;
-		const { abstract } = this.props.abstractData.abstract;
-		const { word } = this.props;
-		const mappedAuthors = authors.map((author, i) =>
-			<IndexLink to="/" key={i} onClick={this.authorRoute.bind(this,author)}>
-				{!!i && ", "}
+  render() {
+    const { authors, conferences, downloadLink, title, doi, frequency } = this.props.article
+    const { bibtex } = this.props.bibtexData.bibtex
+    const { abstract } = this.props.abstractData.abstract
+    const { word } = this.props
+    const { showAbstract, showModal, checked } = this.state
+    const mappedAuthors = authors.map((author, i) =>
+      <IndexLink to="/" key={i} onClick={this.authorRoute.bind(this, author)}>
+				{!!i && ', '}
 				{author}
-			</IndexLink>
-		);
-		const mappedConferences = conferences.map((conference, i) =>
-			<IndexLink to="/" key={i} onClick={this.conferenceRoute.bind(this,conference)}>
-				{!!i && ", "}
+      </IndexLink>
+		)
+    const mappedConferences = conferences.map((conference, i) =>
+      <IndexLink to="/" key={i} onClick={this.conferenceRoute.bind(this, conference)}>
+				{!!i && ', '}
 				{conference}
-			</IndexLink>
-		);
+      </IndexLink>
+		)
 
-		return (
+    return (
       <div className="container" id="article-container">
 
 				{/* BibTeX popup */}
-				<div className="row">
-					<div>
-						<ReactBootstrap.Modal show={this.state.showModal} onHide={this.closeBibtex.bind(this)}>
-							<ReactBootstrap.Modal.Header closeButton>
-								<ReactBootstrap.Modal.Title>BibTeX</ReactBootstrap.Modal.Title>
-							</ReactBootstrap.Modal.Header>
-							<ReactBootstrap.Modal.Body>
-								<pre className="article-modal-pre">{bibtex}</pre>
-							</ReactBootstrap.Modal.Body>
-						</ReactBootstrap.Modal>
-					</div>
-				</div>
+        <div className="row">
+          <div>
+            <ReactBootstrap.Modal show={showModal} onHide={this.closeBibtex}>
+              <ReactBootstrap.Modal.Header closeButton>
+                <ReactBootstrap.Modal.Title>BibTeX</ReactBootstrap.Modal.Title>
+              </ReactBootstrap.Modal.Header>
+              <ReactBootstrap.Modal.Body>
+                <pre className="article-modal-pre">{bibtex}</pre>
+              </ReactBootstrap.Modal.Body>
+            </ReactBootstrap.Modal>
+          </div>
+        </div>
 
 				{/* BibTeX popup */}
-				<div className="row">
-					<div>
-						<ReactBootstrap.Modal show={this.state.showAbstract} onHide={this.closeAbstract.bind(this)}>
-							<ReactBootstrap.Modal.Header closeButton>
-								<ReactBootstrap.Modal.Title>Abstract</ReactBootstrap.Modal.Title>
-							</ReactBootstrap.Modal.Header>
-							<ReactBootstrap.Modal.Body>
-								<pre className="article-modal-pre">
-									<Highlight search={word} matchStyle={{backgroundColor:"#ffd54f"}}>{abstract}</Highlight>
-								</pre>
-								<a target="_blank" href={downloadLink}>
-									<button className="btn btn-primary" id="article-download-button">
-										<span className="glyphicon glyphicon-download"></span> Download
-									</button>
-								</a>
-							</ReactBootstrap.Modal.Body>
-						</ReactBootstrap.Modal>
-					</div>
-				</div>
+        <div className="row">
+          <div>
+            <ReactBootstrap.Modal show={showAbstract} onHide={this.closeAbstract}>
+              <ReactBootstrap.Modal.Header closeButton>
+                <ReactBootstrap.Modal.Title>Abstract</ReactBootstrap.Modal.Title>
+              </ReactBootstrap.Modal.Header>
+              <ReactBootstrap.Modal.Body>
+                <pre className="article-modal-pre">
+                  <Highlight search={word} matchStyle={{ backgroundColor: '#ffd54f' }}>
+                    {abstract}
+                  </Highlight>
+                </pre>
+                <a target="_blank" href={downloadLink}>
+                  <button className="btn btn-primary" id="article-download-button">
+                    <span className="glyphicon glyphicon-download"></span> Download
+                  </button>
+                </a>
+              </ReactBootstrap.Modal.Body>
+            </ReactBootstrap.Modal>
+          </div>
+        </div>
 
 				{/* Article Checkbox + Title */}
-				<div className="row" id="article-title-container">
-					<table>
-						<tr>
-							<th className="article-checkbox-col">
-								<input type="checkbox" ref={doi} value={this.state.checked} id="article-checkbox" onChange={this.handleCheckbox.bind(this)}/>
-							</th>
-							<th className="article-title-col">
-								<a onClick={this.openAbstract.bind(this)}><p id="article-title">{title}</p></a>
-							</th>
-						</tr>
-					</table>
-				</div>
+        <div className="row" id="article-title-container">
+          <table>
+            <tr>
+              <th className="article-checkbox-col">
+                <input type="checkbox" ref={doi} value={checked} id="article-checkbox" onChange={this.handleCheckbox} />
+              </th>
+              <th className="article-title-col">
+                <a onClick={this.openAbstract}><p id="article-title">{title}</p></a>
+              </th>
+            </tr>
+          </table>
+        </div>
 
-	      {/* Word + Occurences */}
-	      <div className="row" id="article-authors-container">
-		      <span id="article-occurences-1">{word}: </span>
-		      <span id="article-occurences-2">Occurs </span>
-		      <span id="article-occurences-1">{frequency} </span>
-		      <span id="article-occurences-2">times.</span>
-	      </div>
+        {/* Word + Occurences */}
+        <div className="row" id="article-authors-container">
+          <span id="article-occurences-1">{word}: </span>
+          <span id="article-occurences-2">Occurs </span>
+          <span id="article-occurences-1">{frequency} </span>
+          <span id="article-occurences-2">times.</span>
+        </div>
 
 				{/* Article Authors */}
-				<div className="row" id="article-authors-container">
-					<span id="article-authors">Authors: </span>
+        <div className="row" id="article-authors-container">
+          <span id="article-authors">Authors: </span>
 					{mappedAuthors}
-				</div>
+        </div>
 
 				{/* Article Conferences */}
-				<div className="row" id="article-authors-container">
-					<span id="article-conferences">Conferences: </span>
+        <div className="row" id="article-authors-container">
+          <span id="article-conferences">Conferences: </span>
 					{mappedConferences}
-				</div>
+        </div>
 
 				{/* Buttons */}
-				<div className="row" id="article-buttons-container">
-					<button className="btn btn-primary" id="article-bibtex-button" onClick={this.openBibtex.bind(this)}>
-						<span className="glyphicon glyphicon-book"></span> BibTeX
-					</button>
-					<a target="_blank" href={downloadLink}>
-						<button className="btn btn-primary" id="article-download-button">
-							<span className="glyphicon glyphicon-download"></span> Download
-						</button>
-					</a>
-				</div>
-			</div>
-		);
-	}
+        <div className="row" id="article-buttons-container">
+          <button className="btn btn-primary" id="article-bibtex-button" onClick={this.openBibtex}>
+            <span className="glyphicon glyphicon-book"></span> BibTeX
+          </button>
+          <a target="_blank" href={downloadLink}>
+            <button className="btn btn-primary" id="article-download-button">
+              <span className="glyphicon glyphicon-download"></span> Download
+            </button>
+          </a>
+        </div>
+      </div>
+		)
+  }
+}
+
+ArticleItem.propTypes = {
+  addToHistory: React.PropTypes.func,
+  generatePapers: React.PropTypes.func,
+  fetchAbstract: React.PropTypes.func,
+  clearAbstract: React.PropTypes.func,
+  fetchBibtex: React.PropTypes.func,
+  onChange: React.PropTypes.func,
+  clearBibtex: React.PropTypes.object,
+  article: React.PropTypes.object,
+  bibtexData: React.PropTypes.object,
+  abstractData: React.PropTypes.object,
+  word: React.PropTypes.string
 }
--- Frontend/dev/js/containers/article-list.js
+++ Frontend/dev/js/containers/article-list.js
@@ -1,177 +1,177 @@
-import React from 'react';
-import ArticleItem from '../components/article-item';
-import { connect } from "react-redux"
-import { bindActionCreators } from 'redux';
-import { fetchArticles } from "../actions/actionCreators";
-import '../../../dev/styles/article-list.sass';
-import FileSaver from "file-saver";
-import { IndexLink } from "react-router";
-import { Button, Glyphicon, MenuItem, ButtonToolbar, Dropdown } from "react-bootstrap";
-const jsPDF = require('jspdf');
+import React from 'react'
+import ArticleItem from '../components/article-item'
+import '../../../dev/styles/article-list.sass'
+import FileSaver from 'file-saver'
+import { IndexLink } from 'react-router'
+import { Button, MenuItem, Dropdown } from 'react-bootstrap'
+const jsPDF = require('jspdf')
 
 export default class ArticleList extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			sortType: 0
-		};
-	}
+  constructor(props) {
+    super(props)
+    this.state = {
+      sortType: 0
+    }
+  }
 
-	/* Automatically fetch articles */
-	// TODO: Rewrite fetch articles to take parameter and use as query parameter.
-	componentWillMount() {
-		this.props.fetchArticles();
-	}
+  /* Automatically fetch articles */
+  // TODO: Rewrite fetch articles to take parameter and use as query parameter.
+  componentWillMount() {
+    this.props.fetchArticles()
+  }
 
-	/*
-	* This function is passed to each article and is called when their checkbox if checked.
-	* It adds a key-value pair to ArticleList's state (key=article doi, value=checkbox status)
-	* This way, when we want to generate a new word cloud based on checked articles we can look
-	* at our current state to see what dois are checked and create a query from that.
-	* */
-	checkArticle(doi,value) {
-		var newState = {};
-		newState[doi] = value;
-		this.setState(newState);
-	}
+  /*
+  * This function is passed to each article and is called when their checkbox if checked.
+  * It adds a key-value pair to ArticleList's state (key=article doi, value=checkbox status)
+  * This way, when we want to generate a new word cloud based on checked articles we can look
+  * at our current state to see what dois are checked and create a query from that.
+  * */
+  checkArticle(doi, value) {
+    const newState = {}
+    newState[doi] = value
+    this.setState(newState)
+  }
 
-	/* Generates query for checked articles */
-	// TODO: Replace Smith w/ doiquery
-	generateWCFromSelected() {
-		console.log(this.state);
-		var doiQuery = ``;
-		var numArticles = 0;
-		const curState = this.state;
-		for (var key in curState) {
-			if (curState.hasOwnProperty(key)) {
-				if (curState[key] === true) {
-					numArticles += 1;
-					doiQuery += `${key} `
-				}
-			}
-		}
-		this.props.addToHistory("Smith", numArticles);
-		this.props.generatePapers("Smith");
-		//this.props.addToHistory(doiQuery, numArticles);
-		//this.props.generatePapers(doiQuery);
-	}
+  /* Generates query for checked articles */
+  // TODO: Replace Smith w/ doiquery
+  /*
+  	this.props.addToHistory(doiQuery, numArticles);
+  	this.props.generatePapers(doiQuery);
+  */
+  generateWCFromSelected() {
+    let doiQuery = ''
+    let numArticles = 0
+    const curState = this.state
+    for (const key in curState) {
+      if (curState.hasOwnProperty(key)) {
+        if (curState[key] === true) {
+          numArticles += 1
+          doiQuery += `${key} `
+        }
+      }
+    }
+    this.props.addToHistory('Smith', numArticles)
+    this.props.generatePapers('Smith')
+  }
 
-	/*
-	* Returns articles list sorted depending on the current state of sort type.
-	* 0: Title
-	* 1: Author
-	* 2: Conference
-	* 3: Frequency
-	* */
-	sortedArticles() {
-		let articles = [];
-		if (this.props.articleData.articles) {
-			articles = this.props.articleData.articles;
-		}
-		switch (this.state.sortType) {
-			case 0:
-				return [...articles].sort((a,b) => a.title > b.title);
-			case 1:
-				return [...articles].sort((a,b) => a.authors[0] > b.authors[0]);
-			case 2:
-				return [...articles].sort((a,b) => a.conferences[0] > b.conferences[0]);
-			case 3:
-				return [...articles].sort((a,b) => a.frequency < b.frequency);
-		}
-		return [];
-	}
+  /*
+  * Returns articles list sorted depending on the current state of sort type.
+  * 0: Title
+  * 1: Author
+  * 2: Conference
+  * 3: Frequency
+  * */
+  sortedArticles() {
+    let articles = []
+    if (this.props.articleData.articles) {
+      articles = this.props.articleData.articles
+    }
+    switch (this.state.sortType) {
+      case 0:
+        return [...articles].sort((a, b) => a.title > b.title)
+      case 1:
+        return [...articles].sort((a, b) => a.authors[0] > b.authors[0])
+      case 2:
+        return [...articles].sort((a, b) => a.conferences[0] > b.conferences[0])
+      case 3:
+        return [...articles].sort((a, b) => a.frequency < b.frequency)
+      default:
+        return []
+    }
+  }
 
-	/* Generates and saves a txt of the article list */
-	listToTXT() {
-		const { articles } = this.props.articleData;
-		const articleArray =	articles.map((article,i) => {
-			const authors = article.authors.join();
-			const conferences = article.conferences.join();
-			return `${i+1}: ${article.title}\nAuthors: ${authors}\nConferences: ${conferences}\n`;
-		});
-		var blob = new Blob(articleArray, {type: "text/plain;charset=utf-8"});
-		FileSaver.saveAs(blob, "article-list.txt");
-	}
+  /* Generates and saves a txt of the article list */
+  listToTXT() {
+    const { articles } = this.props.articleData
+    const articleArray = articles.map((article, i) => {
+      const authors = article.authors.join()
+      const conferences = article.conferences.join()
+      return `${i + 1}: ${article.title}\nAuthors: ${authors}\nConferences: ${conferences}\n`
+    })
+    const blob = new Blob(articleArray, {
+      type: 'text/plain;charset=utf-8'
+    })
+    FileSaver.saveAs(blob, 'article-list.txt')
+  }
 
-	/* Generates and saves a pdf of the article list */
-	listToPDF() {
-		var doc = new jsPDF();
-		const { articles } = this.props.articleData;
-		doc.setFontSize(10);
-		// write articles
-		articles.forEach((article,i) => {
-			const startY = 3*i*8+10;
-			const authors = article.authors.join();
-			const conferences = article.conferences.join();
-			doc.setFontStyle("bold");
-			doc.text((i+1) + ". " + article.title, 10, startY);
-			doc.setFontStyle("normal");
-			doc.text("Authors: " + authors, 10, startY+8);
-			doc.text("Conferences: " + conferences, 10, startY+16);
-		});
-		doc.save('article-list.pdf');
-	}
+  /* Generates and saves a pdf of the article list */
+  listToPDF() {
+    const doc = new jsPDF()
+    const { articles } = this.props.articleData
+    doc.setFontSize(10)
+    // write articles
+    articles.forEach((article, i) => {
+      const startY = 3 * i * 8 + 10
+      const authors = article.authors.join()
+      const conferences = article.conferences.join()
+      doc.setFontStyle('bold')
+      doc.text(`${i + 1}. ${article.title}`, 10, startY)
+      doc.setFontStyle('normal')
+      doc.text(`Authors: ${authors}`, 10, startY + 8)
+      doc.text(`Conferences: ${conferences}`, 10, startY + 16)
+    })
+    doc.save('article-list.pdf')
+  }
 
-	render() {
-		const { word } = this.props.params;
-		const articles = this.sortedArticles();
-		const mappedArticles = articles.map((article,i) =>
-			<li>
-				<ArticleItem {...this.props} key={article.title+i} word={word} onChange={this.checkArticle.bind(this)} article={article}/>
-			</li>
-		);
-		return (
-			<div className="container" id="articles-div">
+  render() {
+    const { word } = this.props.params
+    const articles = this.sortedArticles()
+    const mappedArticles = articles.map((article, i) => <li><ArticleItem {...this.props} key={article.title + i} word={word} onChange={this.checkArticle.bind(this)} article={article} /></li>)
+    return (
+      <div className="container" id="articles-div">
+        <div className="row" id="articles-title-div">
+          <p id="articles-title">
+            {word}
+          </p>
+          <br />
+        </div>
+        <div className="row" id="articles-sorting-div">
+          <Dropdown id="dropdown-toolbar">
+            <Button id="dropdown-colors">
+              Sort By ...
+            </Button>
+            <Dropdown.Toggle bsStyle="success" />
+            <Dropdown.Menu className="dropdown-style">
+              <MenuItem eventKey="1" onClick={() => this.setState({ sortType: 0 })}>
+              Title</MenuItem>
+              <MenuItem eventKey="2" onClick={() => this.setState({ sortType: 1 })}>
+              Authors</MenuItem>
+              <MenuItem eventKey="3" onClick={() => this.setState({ sortType: 2 })}>
+              Conferences</MenuItem>
+              <MenuItem eventKey="4" onClick={() => this.setState({ sortType: 3 })}>
+              Occurences</MenuItem>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+        <div className="row" id="articles-article-list-div">
+          <ul id="articles-list-element">
+            {mappedArticles}
+          </ul>
+        </div>
+        <div className="row" id="articles-generate-wc-button-div">
+          <IndexLink to="/">
+            <button className="btn btn-primary" id="articles-generate-button" onClick={this.generateWCFromSelected.bind(this)}>
+              <span className="glyphicon glyphicon-cloud"></span> Generate Word Cloud From Selected Articles
+            </button>
+          </IndexLink>
+        </div>
+        <div className="row" id="articles-dl-button-div">
+          <button className="btn btn-primary" id="articles-dl-pdf-button" onClick={this.listToPDF.bind(this)}>
+            <span className="glyphicon glyphicon-download"></span> Download List as PDF
+          </button>
+          <button className="btn btn-primary" id="articles-dl-txt-button" onClick={this.listToTXT.bind(this)}>
+            <span className="glyphicon glyphicon-download"></span> Download List as TXT
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
 
-				{/*Word Title*/}
-				<div className="row" id="articles-title-div">
-					<p id="articles-title">{word}</p>
-					<br/>
-				</div>
-
-				{/* Sorting Groups */}
-				<div className="row" id="articles-sorting-div">
-					<Dropdown id="dropdown-toolbar">
-						<Button id="dropdown-colors">
-							Sort By ...
-						</Button>
-						<Dropdown.Toggle bsStyle="success"/>
-						<Dropdown.Menu className="dropdown-style">
-							<MenuItem eventKey="1" onClick={() => this.setState({sortType: 0})}>Title</MenuItem>
-							<MenuItem eventKey="2" onClick={() => this.setState({sortType: 1})}>Authors</MenuItem>
-							<MenuItem eventKey="3" onClick={() => this.setState({sortType: 2})}>Conferences</MenuItem>
-							<MenuItem eventKey="4" onClick={() => this.setState({sortType: 3})}>Occurences</MenuItem>
-						</Dropdown.Menu>
-					</Dropdown>
-				</div>
-
-				{/*Article List*/}
-				<div className="row" id="articles-article-list-div">
-					<ul id="articles-list-element">
-						{mappedArticles}
-					</ul>
-				</div>
-
-				{/*Word CLoud Button*/}
-				<div className="row" id="articles-generate-wc-button-div">
-					<IndexLink to='/'>
-						<button className="btn btn-primary" id="articles-generate-button" onClick={this.generateWCFromSelected.bind(this)}>
-							<span className="glyphicon glyphicon-cloud"></span> Generate Word Cloud From Selected Articles
-						</button>
-					</IndexLink>
-				</div>
-
-				{/*Download Buttons for List*/}
-				<div className="row" id="articles-dl-button-div">
-					<button className="btn btn-primary" id="articles-dl-pdf-button" onClick={this.listToPDF.bind(this)}>
-						<span className="glyphicon glyphicon-download"></span> Download List as PDF
-					</button>
-					<button className="btn btn-primary" id="articles-dl-txt-button" onClick={this.listToTXT.bind(this)}>
-						<span className="glyphicon glyphicon-download"></span> Download List as TXT
-					</button>
-				</div>
-
-			</div>
-		)
-	}
+ArticleList.propTypes = {
+  fetchArticles: React.PropTypes.func,
+  addToHistory: React.PropTypes.func,
+  generatePapers: React.PropTypes.func,
+  articleData: React.PropTypes.object,
+  params: React.PropTypes.object
 }
